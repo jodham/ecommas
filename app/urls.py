27327d54a65686793d237a27c7a09b7aaf8ed012
jodham <wanjalajodham@gmail.com>
2021-03-16 17:59:04 +0300

    update

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from . views import *
+from .views import *
+
 urlpatterns = [
-    path('', myapp, name='myapp'),
+    path('store/', store, name='store'),
+    path('cart/', cart, name='cart'),
+    path('checkout/', checkout, name='checkout')
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from .models import *
 # Create your views here.
-def myapp(request):
-    return render(request, 'index.html'),
+
+
+def store(request):
+    context = {}
+    return render(request, 'app/store.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'app/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'app/checkout.html', context)
+
